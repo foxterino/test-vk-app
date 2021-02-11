77a51666e282c83f906fb1f6d674efc6c0f8d7f9
Foxterino <foxterinogg@gmail.com>
2021-02-11 16:06:35 +0200

    Updates

---
+++ precache-manifest.60a72d9b75108eae742b18ebed792217.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b6f06cbd8c2dcea1805",
+    "url": "/test-vk-app/static/css/main.6911f2ca.chunk.css"
+  },
+  {
+    "revision": "2b6f06cbd8c2dcea1805",
+    "url": "/test-vk-app/static/js/main.8573ff41.chunk.js"
+  },
+  {
+    "revision": "f4119b0dec61e0ca4fb4",
+    "url": "/test-vk-app/static/js/runtime~main.70a241b3.js"
+  },
+  {
+    "revision": "e4a030927f67d38c9fbb",
+    "url": "/test-vk-app/static/css/2.40d7db52.chunk.css"
+  },
+  {
+    "revision": "e4a030927f67d38c9fbb",
+    "url": "/test-vk-app/static/js/2.d1e02a90.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/test-vk-app/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "e2258eec3d41e0a3ea9a6c8b82cd4f75",
+    "url": "/test-vk-app/index.html"
+  }
+];
